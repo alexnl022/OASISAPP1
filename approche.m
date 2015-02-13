bfdbb6eef2c882ff7f56e0bdcd6254dcc50e8579
NAHUM <alexandre.nahum@gmail.com>
2015-02-13 21:10:36 +0100

    Affinage de la fonction approch

--- approche.m
+++ approche.m
@@ -1,11 +1,13 @@
 function [nouv_freq]=approche(anc_freq)
-  nouv_freq=697;
-  diff=abs(anc_freq-697);
-  table=[770,852,941,1209,1336,1477];
-  for i=1:6
-      if abs(table(i)-anc_freq)<diff
-	  diff=abs(table(i)-anc_freq);
+
+  table=[697,770,852,941,1209,1336,1477];
+  nouv_freq=anc_freq;
+  for i=1:7
+      if abs(table(i)-anc_freq)<10
+	  
           nouv_freq=table(i);
       end;
   end;
 end;
+
+disp(approche(735));
